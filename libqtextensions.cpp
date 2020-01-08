3b8fbab4219184ab17da65e9548bb9f985590d61
Hans Dijkema <hans@dykema.nl>
2020-01-08 19:27:51 +0100

    * Added version and licensing information
    Signed-off-by: Hans Dijkema <hans@dykema.nl>

--- libqtextensions.cpp
+++ libqtextensions.cpp
@@ -14,3 +14,30 @@
  *   \endlist
  * \endlist
  */
+
+#include <QObject>
+
+QString QtExtensions::version()
+{
+    QString ver(QObject::tr("QtExtensions version %1.%2").arg(
+                    QString::number(QTEXTENSIONS_VERSION_MAJOR),
+                    QString::number(QTEXTENSIONS_VERSION_MINOR)
+                    )
+                );
+    return ver;
+}
+
+QString QtExtensions::license()
+{
+    return QString(QObject::tr("GNU Lesser General Public License version 3"));
+}
+
+QString QtExtensions::licenseShort()
+{
+    return QString("LGPLv3");
+}
+
+QString QtExtensions::author()
+{
+    return QString("(c) %1 Hans Dijkema").arg(QTEXTENSIONS_DEVEL_YEARS);
+}
--- libqtextensions.h
+++ libqtextensions.h
@@ -3,4 +3,14 @@
 
 #include "libqtextensions_global.h"
 
+class LIBQTEXTENSIONS_EXPORT QtExtensions
+{
+public:
+    QString version();
+    QString license();
+    QString licenseShort();
+    QString author();
+};
+
+
 #endif // LIBQTEXTENSIONS_H
